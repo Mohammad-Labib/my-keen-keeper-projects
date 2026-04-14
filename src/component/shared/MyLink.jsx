1a7d98c1d237d3d484b55f0e1098fd7c8e4517ff
Mohammad-Labib <friendlabib@gmail.com>
2026-04-14 16:50:34 +0600

    somthing add navbar

--- src/component/shared/MyLink.jsx
+++ src/component/shared/MyLink.jsx
@@ -5,13 +5,17 @@ import { usePathname } from "next/navigation";
 
 const MyLink = ({ href, children }) => {
     const pathname = usePathname();
+
+   
     const isActive = pathname === href;
 
     return (
         <Link href={href}>
             <div
                 className={`flex items-center gap-2 px-3 py-2 rounded-md transition ${
-                    isActive ? "bg-green-900 text-white" : "bg-transparent"
+                    isActive
+                        ? "bg-green-900 text-white"
+                        : "bg-transparent"
                 }`}
             >
                 {children}
