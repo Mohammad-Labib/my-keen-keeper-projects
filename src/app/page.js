a3113af83b83330bdcd78556edfffe93d26f6b98
Mohammad-Labib <friendlabib@gmail.com>
2026-04-16 23:01:49 +0600

    add btn click show data

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,15 @@
-import FinalFriendCart from "@/component/FriendCart/FinalFriendCart";
+import Link from "next/link";
 
-const DataPage = async () => {
-
-    const res = await fetch('http://localhost:3000/friendData.json', {
-        cache: "no-store"
-    });
-
-    if (!res.ok) {
-        throw new Error("Failed to fetch data");
-    }
-
-    const data = await res.json();
-
+export default function Home() {
     return (
-        <div>
-            <h2 className="text-2xl font-bold mb-4">
-                Total Friends: {data.length}
-            </h2>
+        <div className="flex justify-center items-center ">
+            
+            <Link href="/dataStor">
+                <button className="btn btn-primary">
+                    Go to Data Page
+                </button>
+            </Link>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                {
-                    data.map(friendCards => (
-                        <FinalFriendCart 
-                            key={friendCards.id}
-                            friendCards={friendCards}
-                        />
-                    ))
-                }
-            </div>
         </div>
     );
-};
-
-export default DataPage;
+}
--- src/component/banner/Banner.jsx
+++ src/component/banner/Banner.jsx
@@ -1,49 +1,63 @@
 import React from 'react';
 import { FaPlus } from 'react-icons/fa';
+import Link from 'next/link';
 
 const Banner = () => {
     return (
-       <section className='container mx-auto mt-10'>
-         <div className="hero ">
-  <div className="hero-content text-center">
-    <div className="max-w-xl">
-      <h1 className="text-3xl font-bold">Friends to keep close in your life</h1>
-      <p className="py-6 text-gray-500">
-        Your personal shelf of meaningful connections. Browse, tend, and nurture the
-relationships that matter most.
-      </p>
-      <button className="btn bg-green-900 text-white "> <FaPlus /> Add a Friend</button>
-    </div>
-  </div>
-</div>
+        <section className='container mx-auto mt-10'>
+            
+            <div className="hero">
+                <div className="hero-content text-center">
+                    <div className="max-w-xl">
 
+                        <h1 className="text-3xl font-bold">
+                            Friends to keep close in your life
+                        </h1>
 
-<div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-6">
+                        <p className="py-6 text-gray-500">
+                            Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.
+                        </p>
 
-    <div className="flex flex-col items-center justify-center w-60 h-30 p-4 bg-white shadow rounded-lg">
-        <h1 className="text-3xl font-bold ">10</h1>
-        <p className="text-gray-600">Total Friends</p>
-    </div>
+                        
+                        <div className='flex justify-center items-center'>
+                            <Link href="/dataStor">
+                            <button className="btn bg-green-900 text-white flex items-center gap-2">
+                                <FaPlus />
+                                Add a Friend
+                            </button>
+                        </Link>
+                        </div>
 
-    <div className="flex flex-col items-center justify-center w-60 h-30 p-4 bg-white shadow rounded-lg">
-        <h1 className="text-3xl font-bold ">3</h1>
-        <p className="text-gray-600">On Track</p>
-    </div>
+                    </div>
+                </div>
+            </div>
 
-    <div className="flex flex-col items-center justify-center w-60 h-30 p-4 bg-white shadow rounded-lg">
-        <h1 className="text-3xl font-bold ">6</h1>
-        <p className="text-gray-600">Need Attention</p>
-    </div> 
+            
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-6">
 
-    <div className="flex flex-col items-center justify-center w-60 h-30 p-4 bg-white shadow rounded-lg">
-        <h1 className="text-3xl font-bold ">12</h1>
-        <p className="text-gray-600">Interactions This Month</p>
-    </div>
+                <div className="flex flex-col items-center justify-center w-60 h-30 p-4 bg-white shadow rounded-lg">
+                    <h1 className="text-3xl font-bold">10</h1>
+                    <p className="text-gray-600">Total Friends</p>
+                </div>
 
-   
+                <div className="flex flex-col items-center justify-center w-60 h-30 p-4 bg-white shadow rounded-lg">
+                    <h1 className="text-3xl font-bold">3</h1>
+                    <p className="text-gray-600">On Track</p>
+                </div>
 
-</div>
-       </section>
+                <div className="flex flex-col items-center justify-center w-60 h-30 p-4 bg-white shadow rounded-lg">
+                    <h1 className="text-3xl font-bold">6</h1>
+                    <p className="text-gray-600">Need Attention</p>
+                </div>
+
+                <div className="flex flex-col items-center justify-center w-60 h-30 p-4 bg-white shadow rounded-lg">
+                    <h1 className="text-3xl font-bold">12</h1>
+                    <p className="text-gray-600">Interactions This Month</p>
+                </div>
+
+            </div>
+
+        </section>
     );
 };
 
